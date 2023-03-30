015a21a0ff6d4978d3df60bc14a335fed1772708
Aidor777 <guillaume.garin@live.com>
2023-03-30 21:50:48 +0200

    Initial project setup

---
+++ src/main/java/com/garing/cucumberj/steps/Application.java
@@ -0,0 +1,11 @@
+package com.garing.cucumberj.steps;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Logger.getGlobal().log(Level.INFO, "Application running");
+    }
+}
---
+++ src/test/java/com/garing/cucumberj/steps/TestApplication.java
@@ -0,0 +1,8 @@
+package com.garing.cucumberj.steps;
+
+public class TestApplication {
+
+    public static void main(String[] args) {
+        assert true : "Can't happen!";
+    }
+}
